1c1dd5a2c2412bba6e56252e4ca273359a87973c
Roman Schindler <romanschindler@hotmail.com>
2016-03-29 09:50:01 +0200

    SVM - First implementation of a svm-classifier - uses built-in svm functions (fitcsvm) with a rbf kernel - one-vs-all technique for multiclass svm-classification - does not yet include cross validation

--- Exercise2_SVM/SVMTrain.m
+++ Exercise2_SVM/SVMTrain.m
@@ -1,9 +1,14 @@
-function [ model ] = SVMTrain( data , numClasses)
-%SVMTRAIN Summary of this function goes here
-%   Detailed explanation goes here
+function [ model ] = SVMTrain( data , numClasses )
+%SVMTRAIN Train a multiclass SVM-classifier on the data
+%   [model] = SVMTrain(data, numClasses) trains multiclass svm-classifers
+%               on the data with numClasses different classes
 
-%not sure if 4 is the right dimension for this
-model = zeros(numClasses, 4);
-
+% Build as many binary svm-classifiers as we have classes (one-vs-all)
+% (first model classifies 0 vs not 0, second classifies 1 vs not 1, etc)
+model = cell(numClasses,1);
+for i = 1:numClasses
+    model{i} = fitcsvm(data(:,2:end), data(:,1) == (i-1),...
+        'Standardize',true,'KernelFunction','RBF', 'KernelScale','auto');
+end
 end
 
--- Exercise2_SVM/SVM_Script.m
+++ Exercise2_SVM/SVM_Script.m
@@ -5,20 +5,20 @@ test = csvread('../test.csv');
 
 %% Train the SVM
 
-n_class = size(unique(train(:,1)));
+n_class = size(unique(train(:,1)),1);
 
 SVMModel = SVMTrain(train, n_class);
 
 %% classify the testset
 
-classes = classifySVM(SVMModel, test(:,2:end));
+classes = classifySVM(SVMModel, test(:,2:end), n_class);
 
 
 %% evaluate accuracy
 
 accur = classes == test(:,1);
 succ = sum(accur==1);
-acurracy = succ/size(accur)
+acurracy = succ/size(accur,1)
 
 
 %% do some cross-validation...

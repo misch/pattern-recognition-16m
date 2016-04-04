69852d35d54aea7b5a4140eef668566693b4cb3c
Phyranja <vita@students.unibe.ch>
2016-04-04 12:20:55 +0200

    Did the cross validation on the MLP and wrote the turn-in file.

--- Exercise2_MLP/MLP_Script.m
+++ Exercise2_MLP/MLP_Script.m
@@ -20,7 +20,7 @@ clear testData testLabels
 setSize = size(allLabels,1);
 results = zeros(7,10,1);
 quadrantSize = floor(setSize/4);
-for numNeurons = 100:400:4000 %4200
+for numNeurons = 10:20:100 %4200
 	numNeurons
 	nextLine = zeros(7,10);
 	for learningRate10 = 2:2:10
@@ -50,7 +50,7 @@ for numNeurons = 100:400:4000 %4200
 	size(results)
 end
 
-%% write result to file
+% write result to file
 
 resultReshaped = reshape(results,7,10*size(results,3))';
 csvwrite('result.csv',resultReshaped);
